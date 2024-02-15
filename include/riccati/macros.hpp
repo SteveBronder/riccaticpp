323df01bb773a3422c063648ac8e3533121e7b49
Steve Bronder <sbronder@flatironinstitute.org>
2024-02-15 12:24:49 -0500

    ci

--- include/riccati/macros.hpp
+++ include/riccati/macros.hpp
@@ -3,38 +3,41 @@
 
 namespace riccati {
 
-#ifdef __GNUC__
+#ifdef __GNUC__ // 1
+
 /**
  * If statements predicate tagged with this attribute are expected to
  * be true most of the time. This effects inlining decisions.
  */
-#ifndef likely
+#ifndef likely // 2
 #define likely(x) __builtin_expect(!!(x), 1)
-#endif
-#ifndef unlikely
+#endif // 2
+
+#ifndef unlikely // 2
 #define unlikely(x) __builtin_expect(!!(x), 0)
-#endif
-#ifdef __has_attribute
-#if __has_attribute(noinline) && __has_attribute(cold)
-#ifndef RICCATI_COLD_PATH
+#endif // 2
+
+#ifdef __has_attribute // 2
+#if __has_attribute(noinline) && __has_attribute(cold) // 3
+#ifndef RICCATI_COLD_PATH // 4
 /**
  * Functions tagged with this attribute are not inlined and moved
  *  to a cold branch to tell the CPU to not attempt to pre-fetch
  *  the associated function.
  */
 #define RICCATI_COLD_PATH __attribute__((noinline, cold))
-#endif
-#endif
-#endif
-#endif
-#ifndef RICCATI_COLD_PATH
+#endif // 4
+#endif // 3
+#endif // 2
+
+#ifndef RICCATI_COLD_PATH // 2
 #define RICCATI_COLD_PATH
-
-#ifndef RICCATI_NO_INLINE
+#endif // 2
+#ifndef RICCATI_NO_INLINE // 2
 #define RICCATI_NO_INLINE __attribute__((noinline))
-#endif
+#endif // 2
 
-#ifndef RICCATI_ALWAYS_INLINE
+#ifndef RICCATI_ALWAYS_INLINE // 2
 #define RICCATI_ALWAYS_INLINE __attribute__((always_inline)) inline
 #endif
 
