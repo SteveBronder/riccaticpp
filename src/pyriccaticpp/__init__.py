5bdb7c5332815ecdd9422fd70bbea9f8f4627282
Steve Bronder <sbronder@flatironinstitute.org>
2024-08-19 17:35:05 -0400

    fix python github actions

--- src/pyriccaticpp/__init__.py
+++ src/pyriccaticpp/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import annotations
 
-from .pyriccaticpp import Init, evolve, choose_osc_stepsize, choose_nonosc_stepsize
-__all__ = ["Init", "evolve", "choose_osc_stepsize", "choose_nonosc_stepsize"]
+from .pyriccaticpp import Init, step, evolve, choose_osc_stepsize, choose_nonosc_stepsize, osc_evolve, nonosc_evolve
+__all__ = ["Init", "step", "evolve", "choose_osc_stepsize", "choose_nonosc_stepsize", "osc_evolve", "nonosc_evolve"]
