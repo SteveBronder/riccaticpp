35678b317389c12c7f88ee666ee43128881de0eb
Steve Bronder <sbronder@flatironinstitute.org>
2024-11-14 16:38:37 -0500

    update benchmarks so that passed data is opened up. Need to still do solve_ivp and passing init func args

--- benchmarks/fig1.py
+++ benchmarks/fig1.py
@@ -10,7 +10,10 @@ import time
 import polars as pl
 from enum import Enum
 import timeit
+import itertools
+import inspect
 
+# %%
 class Algo(Enum):
   PYRICCATICPP = 1
   DOP853 = 2
@@ -21,86 +24,80 @@ class Algo(Enum):
   LSODA = 7
   def __str__(self):
     match self.value:
-        case Algo.PYRICCATICPP:
-            return "pyriccaticpp"
-        case Algo.DOP853:
-            return "DOP853"
-        case Algo.RK45:
-            return "RK45"
-        case Algo.RK23:
-            return "RK23"
-        case Algo.RADAU:
-            return "Radau"
-        case Algo.BDF:
-            return "BDF"
-        case Algo.LSODA:
-            return "LSODA"
-        case _:
-            return "unknown"
+      case Algo.PYRICCATICPP:
+        return "pyriccaticpp"
+      case Algo.DOP853:
+        return "DOP853"
+      case Algo.RK45:
+        return "RK45"
+      case Algo.RADAU:
+        return "Radau"
+      case Algo.BDF:
+        return "BDF"
+      case Algo.LSODA:
+        return "LSODA"
+      case _:
+        return "unknown"
 
 
 class Problem(Enum):
   BREMER237 = 1
   BURST = 2
   AIRY = 3
+  SCHRODINGER = 4
+  FLAME_PROP = 5
   def __str__(self):
-     match self.value:
-        case Problem.BREMER237:
-            return "bremer237"
-        case Problem.BURST:
-            return "burst"
-        case Problem.AIRY:
-            return "airy"
-        case _:
-            return "unknown"
+    match self.value:
+      case Problem.BREMER237:
+        return "bremer237"
+      case Problem.BURST:
+        return "burst"
+      case Problem.AIRY:
+        return "airy"
+      case Problem.SCHRODINGER:
+        return "schrodinger"
+      case Problem.FLAME_PROP:
+        return "flame_prop"
+      case _:
+        return "unknown"
 
 def to_string(algo : Algo):
     match algo:
-        case Algo.PYRICCATICPP:
-            return "pyriccaticpp"
-        case Algo.DOP853:
-            return "DOP853"
-        case Algo.RK45:
-            return "RK45"
-        case Algo.RK23:
-            return "RK23"
-        case Algo.RADAU:
-            return "Radau"
-        case Algo.BDF:
-            return "BDF"
-        case Algo.LSODA:
-            return "LSODA"
-        case _:
-            return "unknown"
+      case Algo.PYRICCATICPP:
+        return "pyriccaticpp"
+      case Algo.DOP853:
+        return "DOP853"
+      case Algo.RK45:
+        return "RK45"
+      case Algo.RK23:
+        return "RK23"
+      case Algo.RADAU:
+        return "Radau"
+      case Algo.BDF:
+        return "BDF"
+      case Algo.LSODA:
+        return "LSODA"
+      case _:
+        return "unknown"
 
-def bremer_w_gen(*kwargs):
-  l = kwargs[0]
-  return lambda x: l * np.sqrt(1 - x**2 * np.cos(3 * x))
-
-def bremer_g(*kwargs):
-   return lambda x: np.zeros_like(x)
-
-# For the reference solution
-def bremer_f_gen(*kwargs):
-  l = kwargs[0]
-  def f(t, y):
-    yp = np.zeros_like(y)
-    yp[0] = y[1]
-    yp[1] = -(l**2) * (1 - t**2 * np.cos(3 * t)) * y[0]
-    return yp
-  return f
-
+def get(dict, *args):
+  if len(args) == 1:
+    return dict[args[0]]
+  else:
+    return get(dict[args[0]], *args[1:])
+    
 def benchmark(method_args, ytrue, errref, N=1000):
   if method_args["method"] is Algo.PYRICCATICPP:
     info = ric.Init(*method_args["method_args"]["init_args"])
+    solver_args = method_args["method_args"]["solver_args"]
     method_args["method_args"]["init_step_args"] = (info,
-                                                    method_args["method_args"]["solver_args"]["xi"],
-                                                    method_args["method_args"]["solver_args"]["xf"],
-                                                    method_args["method_args"]["solver_args"]["epsilon_h"])
+                                                    solver_args["xi"],
+                                                    solver_args["xf"],
+                                                    solver_args["epsilon_h"])
     init_step = ric.choose_nonosc_stepsize(*method_args["method_args"]["init_step_args"])
-    method_args["method_args"]["solver_args"]["init_stepsize"] = init_step
-    runtime = timeit.timeit(lambda: ric.evolve(info=info, **method_args["method_args"]["solver_args"]), number=N) / N
-    _, ys, _, _, _, _, _, _, _ = ric.evolve(info=info, **method_args["method_args"]["solver_args"])
+    solver_args["init_stepsize"] = init_step
+    runtime = timeit.timeit(lambda: ric.evolve(info=info, **solver_args), number=N) / N
+    _, ys, _, _, _, _, _, _, _ = ric.evolve(info=info, **solver_args)
     ys = np.array(ys)
     # Compute statistics
     yerr = np.abs((ytrue - ys[-1]) / ytrue)
@@ -120,13 +117,46 @@ def benchmark(method_args, ytrue, errref, N=1000):
 # Eq. (237) in Bremer 2018.
 # "On the numerical solution of second order ordinary
 #  differential equations in the high-frequency regime".
-reftable = "./benchmarks/data/eq237.csv"
+dir_path = os.getcwd()
+if dir_path.endswith("benchmarks"):
+  reftable = "./data/eq237.csv"
+else:
+  reftable = "./benchmarks/data/eq237.csv"
 try:
   refarray = pl.read_csv(reftable, separator=",")
 except FileNotFoundError:
-  dir_path = os.getcwd()
   print("Current Directory is ", dir_path)
   print("./data/eq237.csv was not found. This script should be run from the top level of the repository.")
+
+def bremer_w_gen(lamb, *args):
+  return lambda x: lamb * np.sqrt(1 - x**2 * np.cos(3 * x))
+
+def bremer_g_gen(*args):
+  return lambda x: np.zeros_like(x)
+
+# For the reference solution
+# TODO: Replace with general Eq with F() and G()
+def bremer_f_gen(lamb, *args):
+  def f(t, y):
+    yp = np.zeros_like(y)
+    yp[0] = y[1]
+    yp[1] = -(lamb**2) * (1 - t**2 * np.cos(3 * t)) * y[0]
+    return yp
+  return f
+
+def bremer_dyi_init(lamb):
+  return complex(lamb)
+
+problem_dictionary = {Problem.BREMER237:
+                   {"range":[-1.0, 1.0],
+                    "yi_init": complex(0.0),  
+                    "dyi_init": bremer_dyi_init,
+                    "w": bremer_w_gen,
+                    "g": bremer_g_gen,
+                    "f": bremer_f_gen,
+                    "data": refarray}}
+
+## Airy
 
 def airy_w_gen(*kwargs):
   return lambda x: np.sqrt(x)
@@ -137,19 +167,13 @@ def airy_g_gen(*kwargs):
 def airy_f_gen(*kwargs):
   return lambda t, y: [t*y[1],y[0]]
 
-
-problem_dictionary = {Problem.BREMER237:
-                   {"range":[-1.0, 1.0],
-                    "init": complex(0.0),
-                    "w": bremer_w_gen,
-                    "g": bremer_g,
-                    "f": bremer_f_gen,
-                    "data": refarray}}
-
-## Airy
+def airy_dyi_init(*kwargs):
+  # NOTE: Hard coded init position as 1 here :(
+  return complex(-sp.airy(-1)[1] - 1j * sp.airy(-1)[3])
 
 problem_dictionary[Problem.AIRY] = {"range":[1, 1e6],
-                                 "init": complex(sp.airy(-1.0)[0] + 1j * sp.airy(-1.0)[2]),
+                                 "yi_init": complex(sp.airy(-1.0)[0] + 1j * sp.airy(-1.0)[2]),
+                                 "dyi_init": airy_dyi_init,
                                  "w": airy_w_gen,
                                  "g": airy_g_gen,
                                  "f": airy_f_gen,
@@ -158,30 +182,71 @@ problem_dictionary[Problem.AIRY] = {"range":[1, 1e6],
 ## Burst
 
 
+## 
+def get_args(func, kwargs):
+  return {k: v for k, v in kwargs.items() if k in inspect.getfullargspec(func).args}
+
+def construct_riccati_args(eps, epsh, n, w, g, range, yi_init, dyi_init, func_data):
+  return {"init_args" : (w(**func_data), 
+                         g(**func_data),\
+                         8, max(32, n), n, n),
+          "solver_args": {"xi": range[0],
+                          "xf": range[1], 
+                          "yi": yi_init,
+                          "dyi": dyi_init(**get_args(dyi_init, func_data)), "eps": eps,
+                          "epsilon_h": epsh, "hard_stop": True}}
+
+def construct_solve_ivp_args(fun, t_span, y0, method, rtol, atol, func_data):
+  return {"fun": fun(func_data),
+          "t_span": t_span,
+          "y0": y0,
+          "method": to_string(method),
+          "rtol": rtol,
+          "atol": atol}
+
+def construct_algo_args(algo:Algo, *args):
+  match algo:
+    case Algo.PYRICCATICPP:
+      # Here it is eps, epsh, n as first 3 of args
+      return construct_riccati_args(args[0][0], args[0][1], args[0][2], 
+                                    **get_args(construct_riccati_args, args[1]), 
+                                    **get_args(construct_riccati_args, args[2]))
+    case _:
+      return construct_solve_ivp_args(args[0], 
+                                    **get_args(construct_solve_ivp_args, args[1]), 
+                                    **get_args(construct_solve_ivp_args, args[2]))
+        
 ## Start Benchmark
+# %%
 epss, epshs, ns = [1e-12, 1e-6], [1e-13, 1e-9], [35, 20]
+atol = 1e-14
+algorithm_dict = {Algo.PYRICCATICPP: [epss, epshs, ns],
+                  Algo.DOP853: [epss],
+                  Algo.RK45: [atol],
+                  Algo.RADAU: [atol],
+                  Algo.BDF: [atol],
+                  Algo.LSODA: [atol]}
+
+                  
 timing_dfs = []
-for key, item in problem_dictionary.items():
-  for (l, ytrue, errref) in item["data"].iter_rows():
-    for eps in epss:
-      for epsh, n in zip(epshs, ns):
-        xi = item["range"][0]
-        xf = item["range"][1]
-        yi = item["init"]
-        method_args = {"method": Algo.PYRICCATICPP,
-                      "function_name" : to_string(key),
-                      "eps": eps,
-                      "method_args" : {
-                          "init_args" : (item["w"](l), item["g"](l), 8, max(32, n), n, n),
-                          "solver_args": {"xi": xi, "xf": xf, "yi": yi,
-                                          "dyi": complex(l), "eps": eps,
-                                          "epsilon_h": epsh, "hard_stop": True}}}
-        timing_dfs.append(benchmark(method_args, ytrue, errref, N=1000))
+for problem_key, problem_item in problem_dictionary.items():
+  for problem_data in problem_item["data"].iter_rows(named=True):
+    print(problem_data)
+    for algo, algo_params in algorithm_dict.items():
+      for algo_iter in list(itertools.product(*algo_params)):
+        method_args = {"method": algo,
+                       "function_name" : to_string(problem_key),
+                       # Eps must always be first arg of tuple
+                       "eps": algo_iter[0],
+                       "method_args" : 
+                        construct_algo_args(algo, algo_iter, problem_item, problem_data)}
+        timing_dfs.append(benchmark(method_args, problem_data["y1"], \
+          problem_data["relative_error"], N=1000))
 
 algo_times = pl.concat(timing_dfs, rechunk=True, how="vertical_relaxed")
 algo_times
 
-
+# %%
 if False:
   print("Runge--Kutta")
   # We're only running this once because it's slow
